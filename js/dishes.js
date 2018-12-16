e2ce063e3b9c0772d030cce86afea6aaae4af9d5
Serhii <serhiimelnyk8@gmail.com>
2018-12-16 03:15:40 +0200

    contact page, pagination

--- js/dishes.js
+++ js/dishes.js
@@ -1,36 +1,69 @@
-// var xhr = new XMLHttpRequest();
+var data = JSON.parse(`[
+    {
+        "name": "Капрезе",
+        "ingredients": "сир моцарела, помідори, базилік, оливковa олія",
+        "weight": "150г",
+        "price": 76,
+        "imgUrl": "../images/caprese.jpg",
+        "category": "італійська, салат"
+    },
+    {
+        "name": "Тірамісу",
+        "ingredients": "сир маскарпоне, цукор, яйця, печево савоярді, заварна кава, ром, какао порошок",
+        "weight": "220г",
+        "price": 70,
+        "imgUrl": "../images/tiramisu.jpg",
+        "category": "італійська, десерт"
+    },
+    {
+        "name": "Піца гавайська",
+        "ingredients": "сир моцарелла, тісто, відварена курка, консервовані ананаси, італійські трави",
+        "weight": "710г",
+        "price": 134,
+        "imgUrl": "../images/hawaiian.jpg",
+        "category": "італійська, піца"
+    }
+]`);
 
-// // 2. Конфигурируем его: GET-запрос на URL 'phones.json'
-// xhr.open('GET', 'data.json', true);
+console.log();
 
-// // 3. Отсылаем запрос
-// xhr.send();
+var filter = decodeURI(document.URL).split("filter=")[1];
+//var dishes = [];
 
-// // 4. Если код ответа сервера не 200, то это ошибка
-// if (xhr.status != 200) {
-//   // обработать ошибку
-//   alert( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
-// } else {
-//   // вывести результат
-//   alert( xhr.responseText ); // responseText -- текст ответа.
+for (var i in data) {
+    if (data[i].category.indexOf(filter) >= 0)
+        //dishes.push(data[i]);
+        createDish(data[i]);
+}
+
+// for (var j in dishes) {
+//     createDish(dishes[j]);
 // }
-// var myRequest = new Request("../data.json");
-// var myMode = myRequest.mode;
 
-// fetch(myRequest)
-// .then(function(response){
-//     alert(response.json());
-// })
+function createDish(dish) {
+    var content = document.getElementById("content");
+    var divDish = document.createElement("div");
+    var image = document.createElement("img");
+    var name = document.createElement("h3");
+    var ingredients = document.createElement("p");
+    var weight = document.createElement("p");
+    var price = document.createElement("p");
 
-var xmlhttp = new XMLHttpRequest();
+    divDish.className += "dish";
 
-xmlhttp.onreadystatechange = function() {
-if (this.readyState == 4 && this.status == 200) {
-    var myObj = JSON.parse(this.responseText);
-    console.log("Json parsed data is: " + JSON.stringify(myObj));
-   }
-};
-xmlhttp.open("GET", "../data.json", true);
-xmlhttp.setRequestHeader('Content-Type', 'aplication/json');
-xmlhttp.setRequestHeader('Access-Control-Allow-Origin', );
-xmlhttp.send();
+    image.src = dish.imgUrl;
+    image.className += "dish-icon";
+
+    name.innerHTML = dish.name;
+    ingredients.innerHTML = "Ігредієнти: " + dish.ingredients;
+
+    weight.innerHTML = "Вага: " + dish.weight;
+    price.innerHTML = "Ціна: " + dish.price + " грн";
+
+    divDish.appendChild(image);
+    divDish.appendChild(name);
+    divDish.appendChild(ingredients);
+    divDish.appendChild(weight);
+    divDish.appendChild(price);
+    content.appendChild(divDish);    
+}
---
+++ js/paginator.js
@@ -0,0 +1,41 @@
+document.getElementsByClassName("paginator")[0].addEventListener("click", pagination)
+
+var dishes = document.getElementsByClassName("dish");
+var count = 2;
+var countPage = Math.ceil(dishes.length / count);
+
+var paginator = document.querySelector(".paginator");
+var page = "";
+for (var i = 0; i < countPage; i++) {
+  page += "<span data-page=" + i + "  id=\"page" + (i + 1) + "\">" + (i + 1) + "</span>";
+}
+paginator.innerHTML = page;
+
+for (var i = 0; i < dishes.length; i++) {
+    if (i < count)
+        dishes[i].style.display = "block";
+}
+
+var mainPage = document.getElementById("page1");
+mainPage.classList.add("pagination-active")
+
+function pagination(event) {
+    var target = event.target;
+    var id = target.id;
+
+    if (target.tagName !== "SPAN") return;
+
+    var dataPage = +target.dataset.page;
+    mainPage.classList.remove("pagination-active");
+    mainPage = document.getElementById(id);
+    mainPage.classList.add("pagination-active");
+
+    for (var i = 0; i < dishes.length; i++) {
+        dishes[i].style.display = "none";
+    }
+
+    for (var i = 0; i < dishes.length; i++) {
+        if (i >= dataPage * count && i < (dataPage + 1) * count)
+            dishes[i].style.display = "block";
+    }
+}
